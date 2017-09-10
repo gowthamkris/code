bd07614bc83d0a74b586cb5458d115f15334eb8a
gowthamkris <30409106+gowthamkris@users.noreply.github.com>
2017-09-10 16:41:16 +0530

    Update amstrong.c

--- amstrong.c
+++ amstrong.c
@@ -1,25 +1,22 @@
-
- 
 #include <stdio.h>
- 
 int main()
 {
-    int n, sum=0, rem=0,temp;
-    scanf("%d", &n);
- 
-    temp=n;
- 
-    while(temp!=0)
-    {
-        rem=temp%10;
-        sum=sum + (rem*rem*rem);
-        temp/=10;
-    }  
- 
-    if(sum==n)  
-        printf("%d",n);
-    else
-        printf("%d",n);
- 
-    return 0;
+    int n, originalNumber, remainder, r = 0;
+    scanf("%d", &n);
+
+    originalNumber = n;
+
+    while (originalNumber != 0)
+    {
+        remainder = originalNumber%10;
+        r += remainder*remainder*remainder;
+        originalNumber /= 10;
+    }
+
+    if(r == n)
+        printf("%d is an Armstrong number.",n);
+    else
+        printf("%d is not an Armstrong number.",n);
+
+    return 0;
 }
